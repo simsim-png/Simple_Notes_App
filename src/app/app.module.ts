67fd23097ffbf1a1992e7d6a42d349bec3fa7a02
Mohammed Safir <msafir@hps-worldwide.com>
2023-11-29 16:19:59 +0100

    Adding directives and Event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NgFor } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgFor
   ],
   providers: [],
   bootstrap: [AppComponent]
