df836f361b07123447741a2b24ca94280a2baf88
Mohammed Safir <msafir@hps-worldwide.com>
2023-12-05 16:02:10 +0100

    Basic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
-import { NgFor } from '@angular/common';
+// import { NgFor } from '@angular/common';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet, Routes, provideRouter } from '@angular/router';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export const routes: Routes = [
+  { path: '', component: NotesListComponent },
+  { path: 'new', component: AddNoteComponent },
+  { path: 'note/:id', component: NoteDetailComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NotesListComponent
+    NotesListComponent,
+    AddNoteComponent,
+    NoteDetailComponent
   ],
   imports: [
     BrowserModule,
-    NgFor
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RouterOutlet,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [provideRouter(routes)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
